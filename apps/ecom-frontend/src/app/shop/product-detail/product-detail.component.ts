8d902ad561f2663272c8c0d75b284c25f9ebefdf
jsan2321 <jsantiago.c@hotmail.com>
2025-04-17 22:07:56 -0500

    add product detail page with related products functionality

---
+++ apps/ecom-frontend/src/app/shop/product-detail/product-detail.component.ts
@@ -0,0 +1,87 @@
+import { Component, effect, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { injectParams } from 'ngxtension/inject-params';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastService } from '../../shared/toast/toast.service';
+import { UserProductService } from '../../shared/service/user-product.service';
+import { Pagination } from '../../shared/model/request.model';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [CommonModule, FaIconComponent, ProductCardComponent],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.scss',
+})
+export class ProductDetailComponent {
+  publicId = injectParams('publicId');
+
+  productService = inject(UserProductService);
+  router = inject(Router);
+  toastService = inject(ToastService);
+
+  lastPublicId = '';
+
+  pageRequest: Pagination = {
+    page: 0,
+    size: 20,
+    sort: [],
+  };
+
+  labelAddToCart = 'Add to cart';
+  iconAddToCart = 'shopping-cart';
+
+  constructor() {
+    effect(() => this.handlePublicIdChange());
+    effect(() => this.handleProductQueryError());
+    effect(() => this.handleRelatedProductQueryError());
+  }
+
+  productQuery = injectQuery(() => ({
+    queryKey: ['product', this.publicId()],
+    queryFn: () =>
+      lastValueFrom(this.productService.findOneByPublicId(this.publicId()!)),
+  }));
+
+  relatedProductQuery = injectQuery(() => ({
+    queryKey: ['related-product', this.publicId(), this.pageRequest],
+    queryFn: () =>
+      lastValueFrom(
+        this.productService.findRelatedProduct(
+          this.pageRequest,
+          this.publicId()!
+        )
+      ),
+  }));
+
+  private handlePublicIdChange() {
+    if (this.publicId()) {
+      if (this.lastPublicId != this.publicId() && this.lastPublicId !== '') {
+        this.relatedProductQuery.refetch();
+        this.productQuery.refetch();
+      }
+      this.lastPublicId = this.publicId()!;
+    }
+  }
+
+  private handleRelatedProductQueryError() {
+    if (this.relatedProductQuery.isError()) {
+      this.toastService.show(
+        'Error! Failed to load related product. please try again.',
+        'ERROR'
+      );
+    }
+  }
+
+  private handleProductQueryError() {
+    if (this.productQuery.isError()) {
+      this.toastService.show(
+        'Error! Failed to load product. please try again.',
+        'ERROR'
+      );
+    }
+  }
+}
